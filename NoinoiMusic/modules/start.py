9743d29e646d24114bb46d36d054adddf66d22a9
Baziibro <96934642+TeamNoinoi@users.noreply.github.com>
2022-08-14 22:17:53 +0530

    Update start.py

--- NoinoiMusic/modules/start.py
+++ NoinoiMusic/modules/start.py
@@ -11,7 +11,7 @@ from NoinoiMusic.config import BOT_USERNAME
 from NoinoiMusic.config import BOT_NAME
 from NoinoiMusic.config import START_IMG
 
-@Client.on_message(command("info") & filters.private & ~filters.edited)
+@Client.on_message(command("start") & filters.private & ~filters.edited)
 async def start_(client: Client, message: Message):
     await message.reply_photo(
         photo=f"{START_IMG}",
